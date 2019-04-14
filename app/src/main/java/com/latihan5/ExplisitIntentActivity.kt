279155fb9b6fd7cd2095817affe142fd73ee3e19
ismiihanum <ismihanum05@gmail.com>
2019-04-14 19:55:48 +0700

    adding scanner

--- app/src/main/java/com/latihan5/ExplisitIntentActivity.kt
+++ app/src/main/java/com/latihan5/ExplisitIntentActivity.kt
@@ -1,12 +1,59 @@
 package com.latihan5
 
-import android.support.v7.app.AppCompatActivity
+import android.hardware.Camera
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.budiyev.android.codescanner.*
+import com.kotlinpermissions.KotlinPermissions
+import java.util.jar.Manifest
+
 
 class ExplisitIntentActivity : AppCompatActivity() {
+
+    private lateinit var codeScanner: CodeScanner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_explisit_intent)
+        val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
+
+        codeScanner = CodeScanner(this, scannerView)
+
+        // Parameters (default values)
+        codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
+        codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
+        // ex. listOf(BarcodeFormat.QR_CODE)
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
+        codeScanner.scanMode = ScanMode.SINGLE // or CONTINUOUS or PREVIEW
+        codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
+        codeScanner.isFlashEnabled = false // Whether to enable flash or not
+
+        // Callbacks
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                Toast.makeText(this, "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
+            }
+        }
+        codeScanner.errorCallback = ErrorCallback {
+            // or ErrorCallback.SUPPRESS
+            runOnUiThread {
+                Toast.makeText(
+                    this, "Camera initialization error: ${it.message}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
     }
+
 }
+
+
+
+
+
+
